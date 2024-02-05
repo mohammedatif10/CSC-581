54181993e5d8f7c0e126f5a06e29d36f5501d561
mohammedatif10 <61773465+mohammedatif10@users.noreply.github.com>
2024-02-05 00:31:28 -0800

    All changes

--- test/MathUtilsTest.java
+++ test/MathUtilsTest.java
@@ -1,18 +1,18 @@
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MathUtilsTest {
-    
+
     private MathUtils mathUtils;
-    
-    @Before
+
+    @BeforeEach
     public void setUp() {
         mathUtils = new MathUtils();
     }
-    
-    @After
+
+    @AfterEach
     public void tearDown() {
         mathUtils = null;
     }
